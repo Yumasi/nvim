4290c8324284b7a86173014fde164e02d5cd9413
Guillaume Pagnoux <guillaume.pagnoux@datadoghq.com>
2023-11-24 14:15:13 +0100

    ui: lualine: add filename
    Signed-off-by: Guillaume Pagnoux <guillaume.pagnoux@datadoghq.com>

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -54,7 +54,8 @@ return {
 
                     lualine_c = {
                         { "diagnostics" },
-                        { "filetype",   icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+                        { "filetype",   icon_only = true,   separator = "", padding = { left = 1, right = 0 } },
+                        { "filename",   file_status = true, path = 1 },
                     },
                     lualine_x = {
                         {
