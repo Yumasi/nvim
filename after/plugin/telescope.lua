b4c80ab9e1f73d9679d5e9cdf8fc86102c43878c
Guillaume Pagnoux <gpagnoux@gmail.com>
2024-01-05 16:56:34 +0000

    telescope: use ivy layout
    Signed-off-by: Guillaume Pagnoux <gpagnoux@gmail.com>

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,8 @@
 local builtin = require('telescope.builtin')
 
 vim.g.mapleader = " "
-vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Files" })
+vim.keymap.set('n', '<leader>pf', builtin.fd, { desc = "Files" })
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = "Search" })
 vim.keymap.set('n', '<leader>bb', builtin.buffers, { desc = "Find buffer" })
 vim.keymap.set('n', '<leader>hm', builtin.man_pages, { desc = "Man pages" })
-vim.keymap.set('n', '<C-p>',      builtin.git_files, { desc = "Browse Git files" })
+vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Browse Git files" })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,22 @@ return {
     {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.4',
-        dependencies = { 'nvim-lua/plenary.nvim' }
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        opts = {
+            pickers = {
+                fd = {
+                    theme = "ivy",
+                },
+                live_grep = {
+                    theme = "ivy",
+                },
+                buffers = {
+                    theme = "ivy",
+                },
+                git_files = {
+                    theme = "ivy",
+                },
+            },
+        },
     },
 }
