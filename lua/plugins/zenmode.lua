098c78c8db57a650b9e511c480bbb581f308d157
Guillaume Pagnoux <gpagnoux@gmail.com>
2024-02-20 10:13:45 +0000

    zenmode: add keybinding and kitty support
    Signed-off-by: Guillaume Pagnoux <gpagnoux@gmail.com>

--- lua/plugins/zenmode.lua
+++ lua/plugins/zenmode.lua
@@ -1,3 +1,13 @@
 return {
     "folke/zen-mode.nvim",
+    opts = {
+        plugins = {
+            kitty = {
+                enabled = true,
+            },
+        },
+    },
+    keys = {
+        { "<leader>tz", vim.cmd.ZenMode, desc = "Toggle zen mode" },
+    },
 }
