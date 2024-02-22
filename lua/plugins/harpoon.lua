1236681431fbe3de68dd1e0dd58c15320cc2cf43
Guillaume Pagnoux <gpagnoux@gmail.com>
2024-02-22 16:11:43 +0000

    make harpoon lazy

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,17 +1,19 @@
 return {
     {
         'theprimeagen/harpoon',
-        init = function()
+        keys = function()
             local mark = require("harpoon.mark")
             local ui = require("harpoon.ui")
 
-            vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon file" })
-            vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Harpoon quick menu" })
+            return {
+                { "<leader>a", mark.add_file,                 { desc = "Harpoon file" } },
+                { "<C-e>",     ui.toggle_quick_menu,          { desc = "Harpoon quick menu" } },
 
-            vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end, { desc = "Show mark 1" })
-            vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end, { desc = "Show mark 2" })
-            vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end, { desc = "Show mark 3" })
-            vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end, { desc = "Show mark 4" })
+                { "<leader>1", function() ui.nav_file(1) end, { desc = "Show mark 1" } },
+                { "<leader>2", function() ui.nav_file(2) end, { desc = "Show mark 2" } },
+                { "<leader>3", function() ui.nav_file(3) end, { desc = "Show mark 3" } },
+                { "<leader>4", function() ui.nav_file(4) end, { desc = "Show mark 4" } },
+            }
         end,
     },
 }
