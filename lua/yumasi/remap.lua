1f26c62f57a4d736556939b6d55649ada8ec7cf2
Guillaume Pagnoux <guillaume.pagnoux@datadoghq.com>
2024-03-25 11:24:58 +0100

    remap: use jk to exit insert mode

--- lua/yumasi/remap.lua
+++ lua/yumasi/remap.lua
@@ -13,3 +13,4 @@ vim.keymap.set("n", "<leader>fd", vim.cmd.Ex, { desc = "Browse current directory
 vim.keymap.set("n", "<leader>fs", vim.cmd.write, { desc = "Save file" })
 vim.keymap.set("n", "<leader>bd", vim.cmd.bdelete, { desc = "Delete buffer" })
 vim.keymap.set("n", "<leader>w", "<C-w>", { noremap = false })
+vim.keymap.set("i", "jk", "<ESC>", { noremap = false })
